b41736256aac9d6d10150eb6959ec2bda74de075
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-28 14:34:42 -0700

    SearchPage.js query string instalado

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,8 +1,27 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import queryString from 'query-string';
 import { HeroCard } from "../components";
 
 export const SearchPage = () => {
 
-  
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const { q = '' } = queryString.parse( location.search );
+
+    
+
+    const { searchText, onInputChange } = useForm({
+      searchText: '',
+    })
+
+    const onSearchSubmit = (event) => {
+      event.preventDefault();
+      if( searchText.trim().length <= 1) return;
+
+      navigate(`?q=${ searchText }`)
+    }
 
     return (
       <>
@@ -13,13 +32,15 @@ export const SearchPage = () => {
           <div className="col-5">
             <h4>Searching</h4>
             <hr />
-            <form>
+            <form onSubmit={ onSearchSubmit }>
               <input
                 type="text"
                 placeholder="Search a hero"
                 className="form-control"
                 name="searchText"
                 autoComplete="off"
+                value={ searchText }
+                onChange={ onInputChange }
               />
 
               <button 
@@ -40,7 +61,7 @@ export const SearchPage = () => {
             </div>
 
             <div className="alert alert-danger">
-              No hero with <b>ABC</b>
+              No hero with <b>{ q }</b>
             </div>
 
             {/*<HeroCard {...hero} />*/ }
